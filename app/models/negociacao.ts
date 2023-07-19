261c9ecac393f378866f2a8c33d17a057711bedd
Leandro Ferreira <leandroferreira281@gmail.com>
2023-07-19 15:14:02 -0300

    Initial commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,10 @@
+export class Negociacao {
+    constructor(
+        public readonly _data: Date, 
+        public readonly _quantidade: number, 
+        public readonly _valor: number){}
+
+    get volume(): number{
+        return this._quantidade*this._valor;
+    }
+}
